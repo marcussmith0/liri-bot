480b2898a96b9c7b720399fabd353a1289782640
Marcus Smith <marcussmith0711@gmail.com>
2017-05-11 19:39:47 -0500

    key added back

--- keys.js
+++ keys.js
@@ -1,1 +1,6 @@
-exports.twitterKeys = {}
+exports.twitterKeys = {
+  consumer_key: 'ZtSTW06eXqjEmLmQ7N83CJ5yS',
+  consumer_secret: 'DK2tfAyzAmQrTr7FFpp6z5FUZFTPRNvuQy53OcPgsH6Vo5WVkS',
+  access_token_key: '861726437887422464-UBdYWNIHzInYjaln86Tgz9G34Lxj7xy',
+  access_token_secret: 'fKd2l7CuKmnnMVvOZgidvjXRtnUTIv7HZwAx53R8exV9O',
+}
